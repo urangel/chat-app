6cf6a68381f5fc90ee6d4e2811a7454943162a4f
Ulises <uli.atx@gmail.com>
2019-08-05 22:17:53 -0500

    react rerenders trigger cascading socket connections

--- client/src/components/MessageBoard.js
+++ client/src/components/MessageBoard.js
@@ -3,17 +3,25 @@ import socketIO_Client from 'socket.io-client'
 
 function MessageBoard() {
 
-  const [messages, setMessages] = useState([]);
-  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]); //empty array is the default state
+  // const [input, setInput] = useState('');
+  const [socket, setSocket] = useState(socketIO_Client("http://localhost:3001"));
+
+  
 
   useEffect( () => {
     console.log('componentDidMount through useEffect!');
-  })
+    socket.on('chat-message', msg =>{
+      setMessages([...messages, msg]);
+    })
+  }, [messages]); //empty array as second argument makes this similar to componentDidMount
 
+  
   const handleSubmit = (e) =>{
     e.preventDefault();
-    setMessages( [...messages, input]);
-    setInput('');
+    socket.emit('chat-message', document.getElementById('message-input').value);
+    // setMessages( [...messages, input]);
+    // setInput('');
   }
 
   return (
@@ -21,8 +29,13 @@ function MessageBoard() {
       <h1>Message Board</h1>
       <ul id='messages'></ul>
       <form>
-        <input type='text' id='message-input' value={input} onChange={ e => setInput(e.target.value)}></input>
+        <input type='text' id='message-input' ></input>
         <button type='submit' onClick={handleSubmit}>Submit</button>
+        {
+          messages.map( msg =>(
+            <li>{msg}</li>
+          ))
+        }
       </form>
     </div>
   )
--- server.js
+++ server.js
@@ -3,22 +3,35 @@ const express = require('express');
 const app = express();
 const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
-const http = require('http');
-const server = http.createServer(app);
-const io = socketIO(server);
+const server = app.listen(PORT);
+const io = socketIO.listen(server);
+const cors = require('cors');
 
 io.on('connection', socket => {
   console.log('connection established');
+  socket.on('chat-message', msg => {
+    console.log('message: ' + msg );
+    io.emit('chat-message', msg);
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  })
 });
+
+// io.emit('connection-message', socket => {
+//   socket.broadcast.emit('Please be nice to each other ✌');
+// })
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
 app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT} 👍`)
-})
